7ea05f774fc41d46224da39c9cc6e3d341653b80
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2021-05-25 17:21:01 +0200

    eclat client

--- eclat.py
+++ eclat.py
@@ -1,3 +1,5 @@
+import calculator_pb2_grpc
+import calculator_pb2
 import grpc
 import argparse
 
@@ -45,3 +47,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# import the generated classes
+
+# open a gRPC channel
+channel = grpc.insecure_channel('localhost:50051')
+
+# create a stub (client)
+stub = calculator_pb2_grpc.CalculatorStub(channel)
+
+# create a valid request message
+number = calculator_pb2.Number(value=16)
+
+# make the call
+response = stub.SquareRoot(number)
+
+# et voilà
+print(response.value)
