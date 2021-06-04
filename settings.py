dc7f861db061ac0ffc976f8602f1eef5aa1cbbcb
Pierpaolo Loreti <pierpaolo.loreti@gmail.com>
2021-06-04 17:34:56 +0000

    add path for AST and FileSystem, add LOAD_DIR for output of hikecc tool

--- settings.py
+++ settings.py
@@ -2,8 +2,16 @@ EXTERNAL_MAKEFILE_PATH= "hike_v3/external/Makefile"
 HIKE_SOURCE_PATH = "hike_v3/src/"
 HIKE_CC = "hike_v3/hike-tools/hikecc.sh"
 BUILD_DIR = "runtime/output/"
+LOAD_DIR = "runtime/output/build/"
 MAP_PATH = "/sys/fs/bpf/maps/hike_chain_map"
+
 BPF_FS_PATH = "/sys/fs/bpf/"
 TRACE_FS_PATH = "/sys/kernel/tracing"
 BPF_FS_PROGS_PATH = BPF_FS_PATH + "progs"
 BPF_FS_MAPS_PATH = BPF_FS_PATH + "maps"
+
+# AST
+HIKE_DATA_LIST = "runtime/hike_programs_ids.csv"
+ECLAT_DATA = "runtime/buildin_eclat_function/"
+HIKE_PROGRAM = "runtime/hike_program/"
+HIKE_REGISTRY = "runtime/registry.csv"
