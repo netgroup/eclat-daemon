1715c38e60182bc72292b45494d5d9d40eba7eee
Pierpaolo Loreti <pierpaolo.loreti@gmail.com>
2021-06-15 06:57:08 +0000

    Fix make_hike_chain()

--- cas.py
+++ cas.py
@@ -88,6 +88,9 @@ def make_hike_chain(makefile, source, hike_dir):
 
     # reset directory
     os.chdir(currentDirectory)
+    
+    # unittest
+    return True
 
 
 def make_ebpf_hike_program(makefile, source, hike_dir):
@@ -96,11 +99,27 @@ def make_ebpf_hike_program(makefile, source, hike_dir):
     $ make -f path-to/hike_vm/external/Makefile -j24 prog PROG=prog.bpf.c HIKE_DIR=path-to/hike_vm/src/
     """
     build_dir = settings.BUILD_DIR
-    cmd = f"make -f {makefile} prog PROG={source} HIKE_DIR={hike_dir} BUILD={build_dir}"
+    # We just need the name of the object file
+    source = source.split("/")[-1]
+
+    # BUILD indicates the path where the compiled file is put and NOT the path of the input file
+    # In external MakeFile, $(shell pwd) is executed to get the path:
+    ## export OUTPUT := $(abspath $(shell pwd)/$(BUILD))
+    # do we need to change folder ???
+    currentDirectory = os.getcwd()
+    os.chdir(build_dir)
+
+    #cmd = f"make -f {makefile} prog PROG={source} HIKE_DIR={hike_dir} BUILD={build_dir}"
+    cmd = f"make -f {makefile} prog PROG={source} HIKE_DIR={hike_dir}"
     os.system(cmd)
 
+    # reset directory
+    os.chdir(currentDirectory)
 
-def hikecc(path_eclat_output, path_chain=settings.MAP_PATH):
+    return True
+
+
+def hikecc(path_eclat_output, map_name):
     """
     # The HIKECC takes as 1) the HIKe Chains object file; 2) the eBPF map
     # that contains all the HIKe Chains; 3) the path of the load script
@@ -113,53 +132,63 @@ def hikecc(path_eclat_output, path_chain=settings.MAP_PATH):
     /bin/bash data/binaries/minimal_chain.hike.load.sh
     """
     # We just need the name of the object file
+    map_name = settings.BPF_FS_MAPS_PATH + "/" + map_name
     path_eclat_output = path_eclat_output.split("/")[-1]
 
     loader_file = path_eclat_output[:-1] + "load.sh"
     hikecc_file = settings.HIKE_CC
     cmd = f"/bin/bash {hikecc_file} {settings.LOAD_DIR + path_eclat_output} " \
-            + f"{path_chain} {settings.LOAD_DIR + loader_file}"
-    os.system(cmd)
-    cmd = f"/bin/bash {settings.LOAD_DIR +  loader_file}"
+            + f"{map_name} {settings.LOAD_DIR + loader_file}"
     os.system(cmd)
 
+    return settings.LOAD_DIR + loader_file
+    
 
-
-
+def load_chain(loader_file):
+    cmd = f"/bin/bash {loader_file}"
+    os.system(cmd)
+    #unittest
+    return True
 
 
 # bpftool prog { load | loadall } OBJ PATH [type TYPE] 
 ## [map {idx IDX |  name  NAME}  MAP] [dev NAME] [pinmaps MAP_DIR]
 def bpftool_prog_loadall(prog_obj, prog_path, type=None, map_idx=None, map_name=None, 
                         map_map=None, dev_name=None, pinmaps_map_dir=None):
-    command = "bpftool prog loadall" + prog_obj + " " + prog_path + " "
+    command = "bpftool prog loadall " + prog_obj + " " + prog_path + " "
     command += bpftool_prog_load_loadall(type, map_idx, map_name, map_map, dev_name, pinmaps_map_dir)
     os.system(command)
+
+    #unittest
+    return True
 
 # bpftool prog { load | loadall } OBJ PATH [type TYPE] 
 ## [map {idx IDX |  name  NAME}  MAP] [dev NAME] [pinmaps MAP_DIR]
 def bpftool_prog_load(prog_obj, prog_path, type=None, map_idx=None, map_name=None, 
                         map_map=None, dev_name=None, pinmaps_map_dir=None):
-    command = "bpftool prog load" + prog_obj + " " + prog_path + " "
+    command = "bpftool prog load " + prog_obj + " " + prog_path + " "
     command += bpftool_prog_load_loadall(type, map_idx, map_name, map_map, dev_name, pinmaps_map_dir)
     os.system(command)
+
+    #unittest
+    return True
 
 def bpftool_prog_load_loadall(type=None, map_idx=None, map_name=None, 
                         map_map=None, dev_name=None, pinmaps_map_dir=None):
     command = ""
     if type:
-        command += "type" + type + " "
+        command += " type " + type + " "
 
     if map_idx:
-        command += "map idx" + map_idx + " " + map_map
+        command += " map idx " + map_idx + " pinned " + map_map
     elif map_name:
-        command += "map name" + map_name + " " + map_map
+        command += " map name " + map_name + " pinned " + map_map
 
     if dev_name:
-        command += "dev" + dev_name + " "
+        command += " dev " + dev_name + " "
 
     if pinmaps_map_dir:
-        command += "pinmaps" + pinmaps_map_dir
+        command += " pinmaps " + pinmaps_map_dir
     return command
 
 
@@ -227,3 +256,6 @@ def bpftool_map_update(map_map, key_data=None, value=None, update_flags=None):
         print("ERRORE")
         
     os.system(command)
+
+    #unittest
+    return True
