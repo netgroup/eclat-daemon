710b03049ef3645d6ca336008860b6b35a66bdb9
Pierpaolo Loreti <pierpaolo.loreti@gmail.com>
2021-06-25 15:47:28 +0000

    eclat_output changed

--- runtime/output/eclat_output.hike.c
+++ runtime/output/eclat_output.hike.c
@@ -1,4 +1,3 @@
-
 #include <stddef.h>
 #include <linux/in.h>
 #include <linux/if_ether.h>
@@ -9,7 +8,6 @@
 
 #define HIKE_DEBUG 1
 #include "hike_vm.h"
-
 #define HIKE_CHAIN_75_ID 75
 #define HIKE_CHAIN_76_ID 76
 
@@ -18,25 +16,27 @@
 #define HIKE_EBPF_PROG_ALLOW_ANY 11
 
 HIKE_CHAIN_1(HIKE_CHAIN_75_ID) {
-	__u64 eth_type;
-	__u64 ttl;
+	__u16 eth_type;
+	__u8 ttl;
 	hike_packet_read_u16(&eth_type, 12);
 	if ( eth_type == 0x86dd ) {
 		hike_elem_call_2(HIKE_EBPF_PROG_DROP_ANY, eth_type);
 		return 0;
 	}
-	ttl = 64;
 	if ( eth_type == 0x800 ) {
+		ttl = 64;
 		hike_packet_write_u8(ttl, 22);
 	}
 	hike_elem_call_2(HIKE_EBPF_PROG_ALLOW_ANY, eth_type);
 
 	return 0;
 }
-
+/*
 HIKE_CHAIN_1(HIKE_CHAIN_76_ID) {
-	__u64 eth_type;
-	__u64 ttl;
+	__u64 a;
+	__u16 eth_type;
+	__u8 ttl;
+	a = 0;
 	hike_packet_read_u16(&eth_type, 12);
 	if ( eth_type == 0x86dd ) {
 		hike_packet_read_u8(&ttl, 21);
@@ -44,11 +44,22 @@ HIKE_CHAIN_1(HIKE_CHAIN_76_ID) {
 			hike_packet_write_u8(17, 21);
 		}
 	}
-	if ( eth_type == 0x800 ) {
-		hike_elem_call_2(HIKE_EBPF_PROG_DROP_ANY, eth_type);
-		return 0;
+	else {
+		if ( eth_type == 0x800 ) {
+			hike_elem_call_2(HIKE_EBPF_PROG_DROP_ANY, eth_type);
+			return 0;
+		}
+		else {
+			if ( eth_type == 0x806 ) {
+				a = 2;
+			}
+			else {
+				a = 3;
+			}
+		}
 	}
-	hike_elem_call_2(HIKE_EBPF_PROG_ALLOW_ANY, eth_type);
+	hike_elem_call_2(HIKE_EBPF_PROG_ALLOW_ANY, a);
 	return 0;
 	
 }
+*/
