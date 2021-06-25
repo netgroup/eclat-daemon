c16730f431efe609ffa8a6158c49f477bbae395a
Pierpaolo Loreti <pierpaolo.loreti@gmail.com>
2021-06-25 15:48:43 +0000

    modify test_parser

--- test/test_parser.py
+++ test/test_parser.py
@@ -4,7 +4,7 @@ from engine import EclatEngine
 
 class TestParser(unittest.TestCase):
     def test_parser_run_success(self):
-        import engine
+        #import engine
         ee = EclatEngine()
         scriptfile = "test/test_eclat_code/test_2.eclat"
         with open(scriptfile, 'r') as f:
