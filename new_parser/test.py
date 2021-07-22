d53a1a6a896d29391b9aafe8b339dff241bdfccf
Pierpaolo Loreti <pierpaolo.loreti@gmail.com>
2021-07-22 15:24:21 +0200

    new parser test 1

--- new_parser/test.py
+++ new_parser/test.py
@@ -1,29 +1,37 @@
 from sly import Lexer, Parser
 
 
-class CalcLexer(Lexer):
-    tokens = {ID, FROM, IMPORT}
-    ignore = ' \t'
-    literals = {'=', '+', '-', '*', '/', '(', ')', '.'}
+class EclatLexer(Lexer):
+    tokens = {ID, FROM, IMPORT, NEWLINE, PASS, WS, SS, COMMA}
+    ignore = '\t'
+    literals = {'=', '+', '-', '*', '/', '(', ')', '.',':'}
 
     # Tokens
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
     ID['from'] = FROM
     ID['import'] = IMPORT
+    ID['pass'] = PASS
+    
+    NEWLINE = r'\n'
+    COMMA =   r','
 
-    ignore_comment = r'\#.*'
-
-    @_(r'\n+')
-    def newline(self, t):
-        self.lineno += t.value.count('\n')
+    ignore_comment = r'[ ]*\043[^\n]*'
 
     def error(self, t):
         print("Illegal character '%s'" % t.value[0])
         self.index += 1
 
+    @_(r' [ ]+')
+    def WS(self, t):
+        return t
+    
+    @_(r' ') # ignore single space
+    def SS(self, t):
+        pass 
 
-class CalcParser(Parser):
-    tokens = CalcLexer.tokens
+
+class EclatParser(Parser):
+    tokens = EclatLexer.tokens
 
     precedence = (
     )
@@ -33,31 +41,63 @@ class CalcParser(Parser):
         self.imports = []
         self.chains = {}
 
-    @_('statement')
+    @_('top_statements', '')
     def program(self, p):
         pass
 
-    @_('import_stmt')
-    def statement(self, p):
+    @_('top_statement top_statements')
+    def top_statements(self, p):
         pass
 
-    @_('IMPORT ID')
+    @_('top_statement')
+    def top_statements(self, p):
+        pass
+
+    @_('import_stmt', 'void_stmt')
+    def top_statement(self, p):
+        pass
+
+    @_('FROM ID IMPORT module_list NEWLINE')
     def import_stmt(self, p):
-        print(p.ID)
+        #print(p)
+        pass
+
+    @_('ID COMMA module_list')
+    def module_list(self, p):
         self.imports.append(p.ID)
+        return p
+    
+    @_('ID')
+    def module_list(self, p):
+        self.imports.append(p.ID)
+        return p
+
+
+    @_('NEWLINE')
+    def void_stmt(self, p):
         pass
 
 
 if __name__ == '__main__':
-    lexer = CalcLexer()
-    parser = CalcParser()
+    lexer = EclatLexer()
+    parser = EclatParser()
     prog = \
         """
-        # test
-        import pippo
-        import pluto
-        """
-    p = parser.parse(lexer.tokenize(prog))
+# test
+from gino import mario
+from pablo import andrea, stefano
+from kilo import etto , grammo
+
+def chain():
+  # commento
+  pass
+
+"""
+    tokens = lexer.tokenize(prog)
+    for tok in tokens:
+        print(tok)
+    tokens = lexer.tokenize(prog)
+    p = parser.parse(tokens)
     print(parser.imports)
     # while True:
     #    try:
