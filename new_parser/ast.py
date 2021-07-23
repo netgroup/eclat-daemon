b9122905da551713813d323147d8a1518a8717c1
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2021-07-23 16:47:14 +0200

    new parser improved. Types are now supported

---
+++ new_parser/ast.py
@@ -0,0 +1,65 @@
+
+from sys import argv
+
+
+class Chain():
+    def __init__(self, name, block):
+        self.name = name
+        self.block = block
+
+    def to_c(self):
+        return self.block.to_c()
+
+
+class Block():
+    def __init__(self, statements):
+        self.statements = statements
+
+    def to_c(self):
+        return "\n".join([s.to_c() for s in self.statements])
+
+
+class Statement():
+    pass
+
+
+class Assigment(Statement):
+    def __init__(self, lvalue, rvalue, ltype=None):
+        self.lvalue = lvalue
+        self.rvalue = rvalue
+        self.ltype = ltype
+
+    def to_c(self):
+        if self.ltype:
+            type = self.ltype.to_c()
+            return f"{type} {self.lvalue} = {self.rvalue};"
+        else:
+            return f"{self.lvalue} = {self.rvalue};"
+
+
+class Pass(Statement):
+    def to_c(self):
+        return ""
+
+
+class Argument():
+    def __init__(self, arg, type=None):
+        self.arg = argv
+        self.type = type
+
+    def to_c(self):
+        if self.type:
+            return f"{self.type} {self.arg}"
+        else:
+            return self.arg
+
+
+class Type():
+    def __init__(self, type):
+        if type in ['u8', 'u16', 'u32', 'u64', 's8', 's16', 's32', 's64']:
+            self.type = type
+        else:
+            raise Exception(f"Unsupported type {self.type}")
+
+    def to_c(self):
+        return self.type
--- new_parser/test.py
+++ new_parser/test.py
@@ -1,3 +1,5 @@
+from ast import *
+#from new_parser.ast import Argument, Statement
 from sly import Lexer, Parser
 import re
 
@@ -27,21 +29,22 @@ class IndentedLexer(Lexer):
             # remove spaces at the end and skip white lines
             line = line.rstrip()
             if not line:
-                #output += '\n'
+                # output += '\n'
                 continue
 
             initial_spaces = len(line) - len(line.lstrip(' '))
             if initial_spaces > curr_indentation_level:
                 indentations.append(initial_spaces)
                 output += INDENT + line + '\n'
-                #print("indenting of {} space".format(initial_spaces))
+                print("indenting of {} space. Current indentation was {}".format(
+                    initial_spaces, curr_indentation_level))
             elif initial_spaces < curr_indentation_level:
                 indentations.pop()
                 expected_spaces = indentations[-1]
                 if expected_spaces != initial_spaces:
                     raise Exception("Syntax error on line {}: expected {} space for deindent ({} received)".format(
                         lineno, expected_spaces, initial_spaces))
-                output += DEDENT + line + '\n'
+                output += DEDENT + '\n' + line + '\n'
             else:
                 output += line + '\n'
             curr_indentation_level = initial_spaces
@@ -49,7 +52,7 @@ class IndentedLexer(Lexer):
         while (len(indentations) > 1):
             indentation = indentations.pop()
             output += DEDENT + '\n'
-        # print(output)
+        print(output)
         return output
 
     def tokenize(self, text):
@@ -64,8 +67,23 @@ class EclatLexer(IndentedLexer):
         IMPORT,
         PASS,
         NAME,
-        NUMBER,  # Python decimals
-        # STRING,  # single quoted strings only; syntax of raw strings
+        # types
+        U8,
+        U16,
+        U32,
+        U64,
+        S8,
+        S16,
+        S32,
+        S64,
+        # constants
+        HEX,
+        FLOAT,
+        INTEGER,
+        STRING,
+        BOOLEAN,
+
+        # NUMBER,  # Python decimals
         LPAR,
         RPAR,
         COLON,
@@ -106,17 +124,32 @@ class EclatLexer(IndentedLexer):
     MULT = r'\*'
     DIV = r'/'
     COMMA = r','
+
+    U8 = r'u8'
+    U16 = r'u16'
+    U32 = r'u32'
+    U64 = r'u64'
+    S8 = r's8'
+    S16 = r's16'
+    S32 = r's32'
+    S64 = r's64',
+    HEX = r'0[xX][0-9a-fA-F]+(?!\w)'
+    FLOAT = r'-?\d+\.\d+'
+    INTEGER = r'-?\d+'
+    STRING = r'(""".*?""")|(".*?")|(\'.*?\')'
+    BOOLEAN = r'true(?!\w)|false(?!\w)'
+
     NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
     ignore_comment = r'[ ]*\043[^\n]*'
 
-    @_(r'0x[0-9a-fA-F]+', r'\d+')
-    def NUMBER(self, t):
-        if t.value.startswith('0x'):
-            t.value = int(t.value[2:], 16)
-        else:
-            t.value = int(t.value)
-        return t
+    # @_(r'0x[0-9a-fA-F]+', r'\d+')
+    # def NUMBER(self, t):
+    #    if t.value.startswith('0x'):
+    #        t.value = int(t.value[2:], 16)
+    #    else:
+    #        t.value = int(t.value)
+    #    return t
 
     # Line number tracking
     # @_(r'\n\s+')
@@ -126,7 +159,7 @@ class EclatLexer(IndentedLexer):
     @_(r'\n')
     def NEWLINE(self, t):
         self.lineno += 1
-        #t.value = 'NEWLINE'
+        # t.value = 'NEWLINE'
         return t
 
     def error(self, t):
@@ -141,9 +174,12 @@ class EclatParser(Parser):
     )
 
     def __init__(self):
-        self.names = {}
         self.imports = []
         self.chains = {}
+        self.loaders = {}
+        # stack for the parser
+        self.statement_list = []
+        self.argument_list = []
 
     @_('top_statements')
     def program(self, p):
@@ -160,23 +196,59 @@ class EclatParser(Parser):
 
     @_('DEF NAME LPAR arglist RPAR COLON NEWLINE block')
     def chain_stmt(self, p):
-        pass
+        c = Chain(p.NAME, p.block)
+        print("Printing {} code:\n".format(p.NAME))
+        print(c.to_c())
+        if p.NAME in self.chains.keys():
+            raise Exception(f"Chain {p.NAME} has already been defined")
+        self.chains[p.NAME] = c
 
     @_('INDENT statements DEDENT')
     def block(self, p):
-        pass
+        b = Block(self.statement_list)
+        self.statement_list = []
+        return b
 
-    @_('statement statements', 'statement')
+    @_('statement NEWLINE statements', 'statement NEWLINE')
     def statements(self, p):
-        pass
+        self.statement_list.append(p.statement)
+        return self.statement_list
 
-    @_('PASS NEWLINE')
+    @_('PASS')
     def statement(self, p):
+        return Pass()
+
+    @_('HEX', 'FLOAT', 'INTEGER', 'STRING', 'BOOLEAN')
+    def const(self, p):
+        return p[0]
+
+    @_('NAME ASSIGN const')
+    def statement(self, p):
+        return Assigment(p.NAME, p.const)
+
+    @_('type COLON NAME ASSIGN const')
+    def statement(self, p):
+        return Assigment(p.NAME, p.const, p.type)
+
+    @_('')
+    def argument(self, p):
         pass
 
-    @_('', 'NAME')
+    @_('NAME')
+    def argument(self, p):
+        self.argument_list.append(Argument(p.NAME))
+
+    @_('argument COMMA arglist', 'argument', '')
     def arglist(self, p):
         pass
+
+    @_('type COLON NAME')
+    def argument(self, p):
+        self.argument_list.append(Argument(p.NAME, p.type))
+
+    @_('U8', 'U16', 'U32', 'U64', 'S8', 'S16', 'S32', 'S64')
+    def type(self, p):
+        return Type(p[0])
 
     @_('FROM NAME IMPORT module_list')
     def import_stmt(self, p):
@@ -184,17 +256,26 @@ class EclatParser(Parser):
         pass
 
     @_('NAME COMMA module_list',
-       'NAME')
+        'NAME')
     def module_list(self, p):
         self.imports.append(p.NAME)
         return p
+
+    ########### TODO #########
+    # expression
+    # statements: if, while, for, return
+
+    # chiamare i moduli
+    # modificare mappe (?)
+
+    # const
+    # variabili globali
 
 
 if __name__ == '__main__':
     lexer = EclatLexer()
     parser = EclatParser()
-    prog = \
-        """
+    prog = """
 # test
 from gino import mario
 from pablo import andrea, stefano
@@ -202,9 +283,12 @@ from kilo import etto , grammo
 
 from package1 import p1
 
-def chain():
-  # commento
+def chain1():
+  a = 2
   pass
+
+def chain2():
+    u8 : b = 7
 
 """
     tokens = lexer.tokenize(prog)
