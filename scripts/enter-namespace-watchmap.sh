7a71654ee5c6502d62e67feef7c4e07055ad195c
Giulio Sidoretti <giuliosidoretti@gmail.com>
2021-12-15 14:45:54 +0000

    improve enter-namespace-watchmap.sh

--- scripts/enter-namespace-watchmap.sh
+++ scripts/enter-namespace-watchmap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nsenter -t $(ps ax | grep e[c]lat | awk '{ print $1 }') -m -n -- bash -c "cd /opt/eclat-daemon && watch -n 0.8 python process_maps.py"
+nsenter -t $(ps ax | grep e[c]latd | awk '{ print $1 }') -m -n -- bash -c "cd /opt/eclat-daemon && watch -n 0.8 python process_maps.py"
