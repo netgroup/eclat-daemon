9bc7f82fc1f1a6266930494abadd92cbb6451fa0
Giulio Sidoretti <giuliosidoretti@gmail.com>
2022-02-14 15:45:18 +0000

    copy-credentials script checks if .ssh folder exists

--- scripts/copy-credentials.sh
+++ scripts/copy-credentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /root/.ssh
+[ -d "/root/.ssh" ] || mkdir /root/.ssh
 cp scripts/temp_credentials/config /root/.ssh
 cp scripts/temp_credentials/github_rsa /root/.ssh
 chmod 600 /root/.ssh/github_rsa
