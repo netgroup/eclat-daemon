cadf8a7e2d98b5092d1f488cdb27db1269e17a7b
Giulio Sidoretti <giuliosidoretti@gmail.com>
2022-03-22 14:37:28 +0000

    enter namespace selects first PID (if more than one)

--- scripts/enter-namespace-debug-no-vm.sh
+++ scripts/enter-namespace-debug-no-vm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nsenter -t $(ps ax | grep e[c]lat |grep -v '\.eclat' | awk '{ print $1 }') -m -n -- bash -c "cd /opt/eclat-daemon && cat /sys/kernel/tracing/trace_pipe | grep -v 'HIKe VM debug'"
+nsenter -t $(ps ax | grep e[c]lat |grep -v '\.eclat' | awk 'NR==1{ print $1 }') -m -n -- bash -c "cd /opt/eclat-daemon && cat /sys/kernel/tracing/trace_pipe | grep -v 'HIKe VM debug'"
