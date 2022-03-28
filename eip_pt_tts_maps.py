18381faae8212e4588674c3f71c43be258757139
Giulio Sidoretti <giuliosidoretti@gmail.com>
2022-03-28 13:19:23 +0000

    EIP-PT: added interface ID and load to maps

--- eip_pt_tts_maps.py
+++ eip_pt_tts_maps.py
@@ -14,6 +14,8 @@ PACKAGE = 'eip'
 PROGRAM = 'mcd'
 MAP = 'eip_mcd_time'
 TEMPLATE = 16
+ID = 0
+LOAD = 0
 map_as_array = []
 
 def parse():
@@ -22,6 +24,8 @@ def parse():
     parser.add_argument("--prog", metavar="prog", default=PROGRAM, help="program name")
     parser.add_argument("--map", metavar="map", default=MAP, help="map name")
     parser.add_argument("--template", metavar="template", type=int, default=TEMPLATE, help="tts template")
+    parser.add_argument("--id", metavar="id", type=int, default=ID, help="interface ID")
+    parser.add_argument("--load", metavar="load", type=int, default=LOAD, help="interface load")
     return parser.parse_args()
 
 args = parse()
@@ -31,8 +35,14 @@ if not os.path.exists(map_path):
 else:
       try :
             template = args.template
+            id = args.id
+            load = args.load
             print(f"template: {template}")
             cal.cal_map_update(map_path, ht.u8(1), template)
+            print(f"ID: {id}")
+            cal.cal_map_update(map_path, ht.u8(2), id)
+            print(f"load: {load}")
+            cal.cal_map_update(map_path, ht.u8(3), load)
             map_as_array = json.loads(cal.bpftool_map_dump(map_path))
             print(f"updated map:\n{map_as_array}")
       except Exception as e:
