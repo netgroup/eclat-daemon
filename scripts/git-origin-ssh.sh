fb299a4f43ec80d663b9e55ff19cf0f014a5650e
Giulio Sidoretti <giuliosidoretti@gmail.com>
2022-05-06 12:40:17 +0000

    closes issue #42

--- scripts/git-origin-ssh.sh
+++ scripts/git-origin-ssh.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 url=$(git remote -v | awk 'NR==1{ print $2 }')
+if [ ${url:0:14} = "git@github.com" ]   # check if origin is already SSH
+then
+    exit 0
+fi
 ssh_url="git@github.com:${url:19}"
 git remote set-url origin $ssh_url
